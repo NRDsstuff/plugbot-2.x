afb527e922fa0b487d75f49f0fba421e013a2db0
NRD <85891552+NRDsstuff@users.noreply.github.com>
2021-07-09 20:55:39 +0200

    Add files via upload

---
+++ install-dependencies.bat
@@ -0,0 +1,10 @@
+@echo off
+npm install discord.js
+npm install discord.js-reaction-menu
+npm install db
+npm install fs
+npm install one-liner-joke
+npm install superagent
+npm install readline
+npm i ffmpeg
+echo required dependencies installed.
---
+++ install-dependencies.sh
@@ -0,0 +1,9 @@
+npm install discord.js
+npm install discord.js-reaction-menu
+npm install db
+npm install fs
+npm install one-liner-joke
+npm install superagent  
+npm install readline
+npm i ffmpeg
+echo required dependencies installed.
